b7bf19d67cf4690485185d3aae5f5e8264ec2307
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-10-13 15:04:35 +0900

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,13 @@ import * as tf from '@tensorflow/tfjs';
 import { predictImages, loadImage } from './predictImageClass';
 import * as comlink from "comlink";
 import { PredictionClass } from './worker';
-let model: tf.LayersModel | undefined;
 
-console.log('start')
-
+console.log('start');
 
 setTimeout( ()=>{
       const imgs = Array.from(document.images).map(image=>image.src);
       load(imgs);
-  },1000);
+  },2000);
 
 async function load(imgs: string[]) {
     const worker = await fetch(chrome.extension.getURL('worker.js'));
@@ -20,8 +18,15 @@ async function load(imgs: string[]) {
     const workerClass: any = comlink.wrap(new Worker(url));
     const instance:PredictionClass = await new workerClass();
     await instance.init(chrome.extension.getURL('model/model.json'));
-
-    console.log(await instance.predictImages(imgs.filter((img,i)=>!!img && i < 10)));
-    console.log(await instance.predictImages(imgs.filter((img,i)=>!!img && (i >= 10 && i < 20))));
+    // await instance.elementTest(Array.from(document.images)[0]);
+    console.log(imgs);
+    await instance.loadImage(imgs);
+    console.log('load done');
+    console.time('load img');
+    // console.log(await instance.predictImages());
+    console.log(await instance.predictImage(1));
+    console.timeEnd('load img');
+    // console.log(await instance.predictImages(imgs.filter((img,i)=>!!img && i < 1)));
+    // console.log(await instance.predictImages(imgs.filter((img)=>!!img)));
 }
 
