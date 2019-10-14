d0e10f03c28a6fa3b36b8281b44273cf77f2a253
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-10-14 19:27:26 +0900

    clear tweet dom

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import * as comlink from "comlink";
-import { PredictionClass } from './worker';
+import { PredictionClass, Predict } from './worker';
 import { Message } from './background';
 
 console.log('start');
-chrome.runtime.onMessage.addListener((message:Message)=>{
+chrome.runtime.onMessage.addListener(async (message:Message)=>{
     if( message.action === 'predict') {
-        const imgs = Array.from(document.images).map(image=>image.src);
-        console.log(imgs.filter((_,index)=>index < 10));
-        load(imgs.filter((_,index)=>index < 10));
+        const imgs = Array.from(document.images)
+        const media = imgs.map(image=>image.src).filter(src=> src.match(/media/));
+        const predicts = await predict(media);
+        setDisplayNoneOnNotIllustOnTwitter(imgs,predicts);
     }
 })
-async function load(imgs: string[]) {
+function setDisplayNoneOnNotIllustOnTwitter(imgs: HTMLImageElement[], predicts?: Predict[]) {
+    if(!predicts) return;
+    Array.from(imgs).forEach(img=>{
+        if( predicts.filter(predict=>predict.src === img.src
+            && predict.predict !== 'illust').length > 0) {
+                const element = img.parentElement;
+                searchParentElement(element!).style.display = 'none';
+            }
+    });
+}
+function searchParentElement(dom: HTMLElement): HTMLElement {
+    let target: HTMLElement = dom;
+    let parent: HTMLElement | null = target.parentElement;
+    while( parent && parent.childElementCount < 6) {
+        target = parent;
+        parent = target.parentElement;
+    }
+    return target;
+}
+async function predict(imgs: string[]): Promise<Predict[] | undefined> {
     const worker = await fetch(chrome.extension.getURL('worker.js'));
     const js = await worker.text();
     const blob = new Blob([js], {type: "text/javascript"});
@@ -19,6 +39,6 @@ async function load(imgs: string[]) {
     const instance:PredictionClass = await new workerClass();
     await instance.init(chrome.extension.getURL('model/model.json'));
     await instance.loadImage(imgs);
-    console.log(await instance.predictImages());
+    return instance.predictImages();
 }
 
